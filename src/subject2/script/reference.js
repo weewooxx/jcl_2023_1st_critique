3283647582f670a7ea006b9b0b1ac44f8702146b
weewooxx <115178109+weewooxx@users.noreply.github.com>
2023-09-05 13:34:06 +0900

    first commit

---
+++ src/subject2/script/reference.js
@@ -0,0 +1,9 @@
+const reference = [
+  {
+    author: "한국여성정책연구원",
+    title: "우리사회의 성별화된 외모 고정관념과 외모강박 실태",
+    year: "２０２０",
+    sourceTitle: "",
+    link: "https://www.kwdi.re.kr/publications/issuePaperView.do?p=1&idx=126206",
+  },
+];
---
+++ src/subject3/script/reference.js
@@ -0,0 +1,9 @@
+const reference = [
+  {
+    author: "요조",
+    title: "에세이 <만지고 싶은 기분>",
+    year: "2023",
+    sourceTitle: "",
+    link: "",
+  },
+];
